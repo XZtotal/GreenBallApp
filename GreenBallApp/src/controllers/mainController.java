4fbd84e7cf1d93a0fd888dbbc8d155e0db3df247
XZtotal <81377833+XZtotal@users.noreply.github.com>
2023-04-29 14:28:23 +0200

    cosas basicas

---
+++ GreenBallApp/src/controllers/mainController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class mainController
+{
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    public void initialize() {
+    }}
