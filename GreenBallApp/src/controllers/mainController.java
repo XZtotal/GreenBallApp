b7b194d7483529be7a53cd05102dcb62a5a6a83a
XZtotal <81377833+XZtotal@users.noreply.github.com>
2023-05-01 21:28:13 +0200

    animacion potente

--- GreenBallApp/src/controllers/mainController.java
+++ GreenBallApp/src/controllers/mainController.java
@@ -1,8 +1,13 @@
 package controllers;
 
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
 public class mainController
 {
@@ -10,7 +15,23 @@ public class mainController
     private TextField usernameField;
     @FXML
     private PasswordField passwordField;
+    @FXML
+    private Button btnEnter;
+    @FXML
+    private HBox hboxcenter;
 
     @FXML
     public void initialize() {
-    }}
+    }
+
+    @FXML
+    public void btnEnterOnAction(ActionEvent actionEvent) {
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), hboxcenter);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0.0);
+
+        fadeTransition.setCycleCount(1);
+        fadeTransition.setAutoReverse(false);
+        fadeTransition.play();
+    }
+}
