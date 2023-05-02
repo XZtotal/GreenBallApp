b722ef49a9ac66948490ac2f8fc27fa6bfc4b513
XZtotal <xztotal@gmail.com>
2023-05-02 10:54:27 +0200

    cosas

--- GreenBallApp/src/controllers/mainController.java
+++ GreenBallApp/src/controllers/mainController.java
@@ -4,6 +4,7 @@ import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
