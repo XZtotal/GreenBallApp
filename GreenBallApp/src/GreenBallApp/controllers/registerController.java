b9c9e7dc0e848e15cba912d088ee7410f3c34668
XZtotal <xztotal@gmail.com>
2023-05-02 12:38:28 +0200

    register v1 y css

--- GreenBallApp/src/GreenBallApp/controllers/registerController.java
+++ GreenBallApp/src/GreenBallApp/controllers/registerController.java
@@ -1,28 +1,20 @@
 package GreenBallApp.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 
 
 public class registerController
 {
-    @javafx.fxml.FXML
-    private HBox hboxcenter;
-    @javafx.fxml.FXML
-    private TextField usernameField;
-    @javafx.fxml.FXML
-    private PasswordField passwordField;
-    @javafx.fxml.FXML
-    private Hyperlink linkRegister;
-    @javafx.fxml.FXML
-    private Button btnEnter;
 
-    @javafx.fxml.FXML
+    @FXML
+    private Button btnReturn;
+
+    @FXML
     public void initialize() {
     }
 
-    @javafx.fxml.FXML
-    public void btnEnterOnAction(ActionEvent actionEvent) {
-    }
+
 }
