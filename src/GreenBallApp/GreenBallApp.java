e302c2b7a60e24d3628d34216d2b3541b6c1fb60
XZtotal <81377833+XZtotal@users.noreply.github.com>
2023-05-05 01:20:21 +0200

    segundo modulo de register.fxml diseñado

--- src/GreenBallApp/GreenBallApp.java
+++ src/GreenBallApp/GreenBallApp.java
@@ -38,6 +38,8 @@ public class GreenBallApp extends Application {
         //     - se muestra el stage de manera no modal mediante el metodo show()
         stage.setScene(scene);
         stage.setTitle("GreenBallApp > Inicio");
+        stage.setMinWidth(600);
+        stage.setMinHeight(500);
         stage.show();
     }
 
