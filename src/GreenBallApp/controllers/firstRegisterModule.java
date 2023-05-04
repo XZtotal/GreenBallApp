7d3226971eb7124fb1a4db5c542228012e101d69
XZtotal <xztotal@gmail.com>
2023-05-04 20:12:58 +0200

    Cambio en la estructura del proyecto 2

---
+++ src/GreenBallApp/controllers/firstRegisterModule.java
@@ -0,0 +1,16 @@
+package GreenBallApp.controllers;
+
+import javafx.scene.control.TextField;
+
+public class firstRegisterModule
+{
+    @javafx.fxml.FXML
+    private TextField fieldUsername;
+    @javafx.fxml.FXML
+    private TextField fieldUsername1;
+    @javafx.fxml.FXML
+    private TextField fieldUsername11;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/GreenBallApp/controllers/registerController.java
@@ -0,0 +1,40 @@
+package GreenBallApp.controllers;
+
+import GreenBallApp.GreenBallApp;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+
+public class registerController
+{
+
+    @FXML
+    private Button btnReturn;
+    @FXML
+    private BorderPane bpContainer;
+
+    @FXML
+    public void initialize() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../interfaces/secondRegisterModule.fxml"));
+        Parent root = loader.load();
+        bpContainer.setCenter(root);
+
+
+    }
+
+
+    @FXML
+    public void btnReturnOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../interfaces/main.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        GreenBallApp.setScene(scene);
+    }
+}
