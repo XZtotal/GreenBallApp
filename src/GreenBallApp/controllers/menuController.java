c843edb697ccc15dd02932df12edeb80f1af4a57
pabloraga <pabloragariera@hotmail.es>
2023-05-09 10:41:37 +0200

    actualizo el menu

--- src/GreenBallApp/controllers/menuController.java
+++ src/GreenBallApp/controllers/menuController.java
@@ -7,8 +7,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.event.ActionEvent;
+import model.Club;
+import model.ClubDAOException;
+import model.Member;
 
 import java.io.IOException;
 
@@ -22,12 +28,22 @@ public class menuController {
     private Button reservas;
     @FXML
     private Button reservar;
+    @FXML
+    private ImageView MiFoto;
+    @FXML
+    private Label Bienvenida;
 
     @javafx.fxml.FXML
     public void btnReturnOnAction(ActionEvent actionEvent) {
     }
     @FXML
     public void initialize() {
+        try {
+            welcomeMessage();
+            showImage();
+        } catch (ClubDAOException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -47,5 +63,21 @@ public class menuController {
     }
     // Escribe un metodo que escriba en el label welcome un mensaje de bienvenida a cada usuario
 
+    public void welcomeMessage() throws ClubDAOException, IOException {
+       Club club = Club.getInstance();
+       Member currentMember = GreenBallApp.member;
+       String nombre = currentMember.getName();
+       Bienvenida.setText("Bienvenido " + nombre);
+    }
+
+    // Escribe un metodo que muestre la imagen del usuario
+
+    public void showImage() throws ClubDAOException, IOException {
+        Club club = Club.getInstance();
+        Member currentMember = GreenBallApp.member;
+        Image foto = currentMember.getImage();
+        MiFoto.setImage(foto);
+
+    }
 
 }
