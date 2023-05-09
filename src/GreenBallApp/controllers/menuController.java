7ceae9cc332438d995faa7db4bdfc21bb0cd953d
pabloraga <pabloragariera@hotmail.es>
2023-05-09 09:53:34 +0200

    alo

--- src/GreenBallApp/controllers/menuController.java
+++ src/GreenBallApp/controllers/menuController.java
@@ -1,11 +1,16 @@
 package GreenBallApp.controllers;
 
+import GreenBallApp.GreenBallApp;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.BorderPane;
 import javafx.event.ActionEvent;
 
+import java.io.IOException;
 
 
 public class menuController {
@@ -14,9 +19,9 @@ public class menuController {
     @FXML
     private Hyperlink cuenta;
     @FXML
-    private Button books;
+    private Button reservas;
     @FXML
-    private Button booking;
+    private Button reservar;
 
     @javafx.fxml.FXML
     public void btnReturnOnAction(ActionEvent actionEvent) {
@@ -29,12 +34,16 @@ public class menuController {
     public void cuentaOnAction(ActionEvent actionEvent) {
     }
 
+
+
     @FXML
-    public void booksOnAction(ActionEvent actionEvent) {
+    public void reservasOnAction(ActionEvent actionEvent) throws IOException {
+
+
     }
 
     @FXML
-    public void bookingOnAction(ActionEvent actionEvent) {
+    public void reservarOnAction(ActionEvent actionEvent) {
     }
     // Escribe un metodo que escriba en el label welcome un mensaje de bienvenida a cada usuario
 
