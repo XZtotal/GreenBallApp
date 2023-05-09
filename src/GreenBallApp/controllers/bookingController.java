733e6b97c0b13a7d9760491f335a6de261aec66d
pabloraga <pabloragariera@hotmail.es>
2023-05-09 10:55:32 +0200

    actualizo el menu

---
+++ src/GreenBallApp/controllers/bookingController.java
@@ -0,0 +1,41 @@
+package GreenBallApp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+
+
+
+public class bookingController
+{
+    @javafx.fxml.FXML
+    private HBox hboxCenter;
+    @javafx.fxml.FXML
+    private TextField fieldUsername;
+    @javafx.fxml.FXML
+    private PasswordField fieldPassword;
+    @javafx.fxml.FXML
+    private Hyperlink linkRegister;
+    @javafx.fxml.FXML
+    private Label Error;
+    @javafx.fxml.FXML
+    private Button btnEnter;
+    @javafx.fxml.FXML
+    private Button btnBook;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void registerOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void btnEnterOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewBookingOnAction(ActionEvent actionEvent) {
+    }
+}
