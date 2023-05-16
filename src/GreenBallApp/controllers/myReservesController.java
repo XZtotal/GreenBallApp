e443a47cc984ac3d865dc633db49bc367db41941
alambar <lambaraa03@gmail.com>
2023-05-16 11:24:39 +0200

    Diseñada clase my reserves

--- src/GreenBallApp/controllers/myReservesController.java
+++ src/GreenBallApp/controllers/myReservesController.java
@@ -1,39 +1,45 @@
 package GreenBallApp.controllers;
 
+import GreenBallApp.GreenBallApp;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 
 public class myReservesController
 {
+
     @javafx.fxml.FXML
-    private HBox hboxCenter;
+    private Button btnReturn;
     @javafx.fxml.FXML
-    private TextField fieldUsername;
+    private TableColumn NickName;
     @javafx.fxml.FXML
-    private PasswordField fieldPassword;
+    private TableColumn Pista;
     @javafx.fxml.FXML
-    private Hyperlink linkRegister;
+    private TableColumn HoraInicio;
     @javafx.fxml.FXML
-    private Label Error;
-    @javafx.fxml.FXML
-    private Button btnEnter;
-    @javafx.fxml.FXML
-    private Button btnBook;
+    private TableColumn HoraFin;
 
     @javafx.fxml.FXML
     public void initialize() {
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void registerOnAction(ActionEvent actionEvent) {
     }
 
     @javafx.fxml.FXML
     public void btnEnterOnAction(ActionEvent actionEvent) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../interfaces/menu.fxml.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        GreenBallApp.setScene(scene);
+
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void viewBookingOnAction(ActionEvent actionEvent) {
     }
 }
