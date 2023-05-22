fd57af76397132e0cc023dca1e18d50794f44ab1
pabloraga <pabloragariera@hotmail.es>
2023-05-22 18:34:34 +0200

    public reserves terminar de meterlo en el gridpane

--- src/GreenBallApp/controllers/publicReservesController.java
+++ src/GreenBallApp/controllers/publicReservesController.java
@@ -12,11 +12,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import model.*;
 
 import java.io.IOException;
@@ -29,13 +28,84 @@ public class publicReservesController
 {
     @javafx.fxml.FXML
     private Button btnReturn;
+    @FXML
+    private GridPane tabla;
+    @FXML
+    private RowConstraints col1;
+    @FXML
+    private RowConstraints col2;
+    @FXML
+    private RowConstraints col3;
+    @FXML
+    private RowConstraints col4;
+    @FXML
+    private RowConstraints col5;
+    @FXML
+    private RowConstraints col6;
+    @FXML
+    private RowConstraints col7;
 
+    int numRow = 13;
+    @FXML
+    private DatePicker fecha;
 
     @FXML
-     public void initialize() {
+     public void initialize() throws ClubDAOException, IOException {
+
+            LocalDate date = fecha.getValue();
+
+            for(int i = 0; i < 6; i++){
+
+               String courtName = "Pista " + (i+1);
+               ArrayList<Booking> courtBookings = (ArrayList<Booking>) Club.getInstance().getCourtBookings(courtName, date);
+
+               for(int j = 9; j <= 21; j++){
+
+                   LocalTime time = LocalTime.of(j, 0);
+
+                   HBox hbox = new HBox();
+                   hbox.setAlignment(javafx.geometry.Pos.CENTER);
+                   Label label = new Label();
+                   label.setFont(new javafx.scene.text.Font(12.0));
+                   hbox.getChildren().add(label);
+
+                     if(courtBookings.size() > 0){
+                          for(Booking booking : courtBookings){
+                            if(booking.getFromTime().equals(time)){
+                                 label.setText(String.valueOf(booking.getMember().getNickName()));
+                                 label.setTextFill(Color.WHITE);
+                                 hbox.setStyle("-fx-background-color: #FF0000");
+                            }else{
+                                label.setText("Libre");}
+                          }
+                     }
 
 
-    }
+
+
+
+               }
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
 
 
 
