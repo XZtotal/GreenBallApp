62c617d08b55e7b40473c0ec0479c5d72d483306
XZtotal <xztotal@gmail.com>
2023-05-26 22:11:16 +0200

    inicio de config

---
+++ src/GreenBallApp/controllers/configController.java
@@ -0,0 +1,33 @@
+package GreenBallApp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
+public class configController
+{
+    @javafx.fxml.FXML
+    private Label labelTop;
+    @javafx.fxml.FXML
+    private BorderPane bpContainer;
+    @javafx.fxml.FXML
+    private HBox hboxDown;
+    @javafx.fxml.FXML
+    private Button btnLast;
+    @javafx.fxml.FXML
+    private Button btnNext;
+    @javafx.fxml.FXML
+    private Label labelDown;
+    @javafx.fxml.FXML
+    private Button btnReturn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void btnReturnOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/GreenBallApp/controllers/configModuleController.java
@@ -0,0 +1,21 @@
+package GreenBallApp.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class configModuleController
+{
+    @javafx.fxml.FXML
+    private Button btnIndentify;
+    @javafx.fxml.FXML
+    private Button btnSecurity;
+    @javafx.fxml.FXML
+    private Button btnCreditCard;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+
+
+}
